8a1b86a8ee023a5cc9525d5d5c3112bb9c000321
elis-elis <elisprostotak@gmail.com>
2024-07-28 15:13:49 +0200

    Added trailer_links to movies_web_generator.py, modified style.css

--- movies_web_generator.py
+++ movies_web_generator.py
@@ -1,3 +1,19 @@
+trailer_links = {
+    "Control": "https://www.youtube.com/watch?v=xUz6y6ANIgE",
+    "Clerks": "https://www.youtube.com/watch?v=Mlfn5n-E2WE",
+    "La haine": "https://www.youtube.com/watch?v=OfE0o9B3dhI",
+    "Eraserhead": "https://www.youtube.com/watch?v=7WAzFWu2tVw",
+    "The Aerial": "https://www.youtube.com/watch?v=Jr4SiSeSzTI",
+    "The Man Who Wasn't There": "https://www.youtube.com/watch?v=htxvLcSnOU0",
+    "Natural Born Killers": "https://www.youtube.com/watch?v=XpLKNclOtLg",
+    "American History X": "https://www.youtube.com/watch?v=LZGVcd5clgg",
+    "Frances Ha": "https://www.youtube.com/watch?v=YdxCnCvCngk",
+    "Following": "https://www.youtube.com/watch?v=62TTN6gD2So",
+    "To Kill a Mockingbird": "https://www.youtube.com/watch?v=KR7loA_oziY",
+    "Psycho": "https://www.youtube.com/watch?v=DTJQfFQ40lI"
+}
+
+
 def read_template():
     with open('_static/index_template.html', 'r') as content:
         return content.read()
@@ -7,13 +23,15 @@ def generate_movie_info(movie_name, details):
     """
     Generates HTML for a single movie.
     """
+    trailer_url = trailer_links.get(movie_name, '#')  # Use '#' as a placeholder if trailer_url is missing
     return f""" 
-    <li class= movie"> 
-        <img src="{details['poster_url']}" alt="{movie_name}">
-        <div>
-            <h2>{movie_name}</h2>
-            <p><strong>Year</strong> {details['year']}</p>
-            <p><strong>Rating</strong> {details['rating']}</p>
+    <li> 
+        <img class="movie-poster" src="{details['poster_url']}" alt="{movie_name}">
+        <div class="movie">
+            <h2 class="movie-title">{movie_name}</h2>
+            <p class="movie-year">Year <strong>{details['year']}</strong></p>
+            <p class="movie-rating">Rating <strong>{details['rating']}</strong></p>
+            <a class="trailer-link" href="{trailer_url}" target="_blank">trailer</a>
         </div>
     </li>
     """
@@ -38,6 +56,3 @@ def write_to_html_file(content):
     """
     with open('_static/movies.html', 'w') as new_file:
         return new_file.write(content)
-
-
-
